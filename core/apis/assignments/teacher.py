e1572d035a083610a5c56557adb5aa70283d4c43
Emma Paris <73710723+anonymous-ait@users.noreply.github.com>
2024-01-12 15:51:32 +0530

    initial changes

--- core/apis/assignments/teacher.py
+++ core/apis/assignments/teacher.py
@@ -9,26 +9,28 @@ teacher_assignments_resources = Blueprint('teacher_assignments_resources', __nam
 
 
 @teacher_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
-@decorators.authenticate_principal
+@decorators.auth_principal
 def list_assignments(p):
     """Returns list of assignments"""
-    teachers_assignments = Assignment.get_assignments_by_teacher(p.teacher_id)
-    teachers_assignments_dump = AssignmentSchema().dump(teachers_assignments, many=True)
-    return APIResponse.respond(data=teachers_assignments_dump)
+    teacher_assignments = Assignment.get_assignments_by_teacher(p.teacher_id)
+    teacher_assignments_dump = AssignmentSchema().dump(teacher_assignments, many=True)
+    return APIResponse.respond(data=teacher_assignments_dump)
 
 
 @teacher_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
 @decorators.accept_payload
-@decorators.authenticate_principal
+@decorators.auth_principal
 def grade_assignment(p, incoming_payload):
     """Grade an assignment"""
     grade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
-
-    graded_assignment = Assignment.mark_grade(
+    grade_assignment = Assignment.give_assignment_grade(
         _id=grade_assignment_payload.id,
         grade=grade_assignment_payload.grade,
-        auth_principal=p
+        principal=p
     )
     db.session.commit()
-    graded_assignment_dump = AssignmentSchema().dump(graded_assignment)
-    return APIResponse.respond(data=graded_assignment_dump)
+    grade_assignment_dump = AssignmentSchema().dump(grade_assignment)
+    return APIResponse.respond(data=grade_assignment_dump)
+
+
+
--- core/models/assignments.py
+++ core/models/assignments.py
@@ -1,6 +1,6 @@
 import enum
 from core import db
-from core.apis.decorators import AuthPrincipal
+from core.apis.decorators import Principal
 from core.libs import helpers, assertions
 from core.models.teachers import Teacher
 from core.models.students import Student
@@ -60,34 +60,45 @@ class Assignment(db.Model):
         return assignment
 
     @classmethod
-    def submit(cls, _id, teacher_id, auth_principal: AuthPrincipal):
+    def submit(cls, _id, teacher_id, principal: Principal):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
-        assertions.assert_valid(assignment.student_id == auth_principal.student_id, 'This assignment belongs to some other student')
+        assertions.assert_valid(assignment.student_id == principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
-
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.DRAFT,'only a draft assignment can be submitted')
+        
         assignment.teacher_id = teacher_id
-        db.session.flush()
-
-        return assignment
-
-
-    @classmethod
-    def mark_grade(cls, _id, grade, auth_principal: AuthPrincipal):
-        assignment = Assignment.get_by_id(_id)
-        assertions.assert_found(assignment, 'No assignment with this id was found')
-        assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
-
-        assignment.grade = grade
-        assignment.state = AssignmentStateEnum.GRADED
+        assignment.state = AssignmentStateEnum.SUBMITTED
         db.session.flush()
 
         return assignment
 
     @classmethod
     def get_assignments_by_student(cls, student_id):
-        return cls.filter(cls.student_id == student_id).all()
+        student_data=cls.filter(cls.student_id == student_id).all()
 
+        assertions.assert_valid(len(student_data) > 0 ,'student does not exists in database')
+        
+        return student_data
+    
     @classmethod
     def get_assignments_by_teacher(cls, teacher_id):
-        return cls.filter(cls.teacher_id == teacher_id).all()
+        teacher_data=cls.filter(cls.teacher_id == teacher_id).all()
+
+        assertions.assert_valid(len(teacher_data) > 0 ,'teacher does not exists in database')
+        return teacher_data
+    
+    @classmethod
+    def give_assignment_grade(cls,_id, grade, principal: Principal):
+        assignment = Assignment.get_by_id(_id)
+        assertions.assert_found(assignment, 'No assignment with this id was found')
+        assertions.assert_valid(assignment.teacher_id == principal.teacher_id, 'This assignment belongs to some other teacher')
+        assertions.assert_valid(grade in [Grade.name for Grade in GradeEnum], 'invalid assignment grade given')
+        assertions.assert_valid(assignment.state == AssignmentStateEnum.SUBMITTED,'given assignment is not submitted')
+
+        assignment.grade = GradeEnum[grade]
+        db.session.flush()
+
+        return assignment
+    
+
